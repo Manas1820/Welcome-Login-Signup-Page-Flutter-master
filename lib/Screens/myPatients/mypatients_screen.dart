073ea11e342ebb2e202c27ddd3efa692c93f46d2
Tanmay Agrawal <tanmaytanu.10@gmail.com>
2020-11-01 13:51:01 +0530

    firebase auth

--- lib/Screens/myPatients/mypatients_screen.dart
+++ lib/Screens/myPatients/mypatients_screen.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/Screens/myPatients/components/body.dart';
 import 'package:flutter_auth/constants.dart';
 import 'package:flutter_auth/models/patients.dart';
 import 'package:flutter_auth/services/patient_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class MyPatientsScreen extends StatefulWidget {
   @override
@@ -14,6 +16,14 @@ class _MyPatientsScreenState extends State<MyPatientsScreen> {
   var _disease = TextEditingController();
   var _patient = Patients();
   var _patientservice = PatientService();
+  final _auth = FirebaseAuth.instance;
+  List<Patients> _patientList = List<Patients>();
+
+  var _editPatientName = TextEditingController();
+
+  var _editPatientDisease = TextEditingController();
+
+  var patient;
 
   @override
   void initState() {
@@ -21,10 +31,21 @@ class _MyPatientsScreenState extends State<MyPatientsScreen> {
     getAllPatients();
   }
 
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   getAllPatients() async {
+    _patientList = List<Patients>();
     var patients = await _patientservice.getPatients();
     patients.forEach((patient) {
-      print(patient['name']);
+      setState(() {
+        var model = Patients();
+        model.name = patient['name'];
+        model.id = patient['id'];
+        model.disease = patient['disease'];
+
+        _patientList.add(model);
+      });
+
+      print('Disease: ' + patient['disease']);
     });
   }
 
@@ -41,11 +62,14 @@ class _MyPatientsScreenState extends State<MyPatientsScreen> {
                   _patient.disease = _disease.text;
                   var result = await _patientservice.savePatient(_patient);
                   print(result);
+                  if (result > 0) Navigator.pop(context);
                 },
                 child: Text('Save'),
               ),
               FlatButton(
-                onPressed: null,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
                 child: Text('Cancel'),
               )
             ],
@@ -68,9 +92,102 @@ class _MyPatientsScreenState extends State<MyPatientsScreen> {
         });
   }
 
+  _editPatientDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (param) {
+          return AlertDialog(
+            actions: [
+              FlatButton(
+                onPressed: () async {
+                  _patient.id = patient[0]['id'];
+                  _patient.name = _editPatientName.text;
+                  _patient.disease = _editPatientDisease.text;
+                  var result = await _patientservice.updatePatient(_patient);
+                  if (result > 0) {
+                    Navigator.pop(context);
+                    getAllPatients();
+                    _showSackbar(
+                      Text('Success'),
+                    );
+                  }
+                  print(result);
+                },
+                child: Text('Update'),
+              ),
+              FlatButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Cancel'),
+              )
+            ],
+            title: Text('Edit Patient Details'),
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TextField(
+                    controller: _editPatientName,
+                    decoration: InputDecoration(hintText: 'Name'),
+                  ),
+                  TextField(
+                    controller: _editPatientDisease,
+                    decoration: InputDecoration(hintText: 'Disease'),
+                  )
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  _editPatient(BuildContext context, patientId) async {
+    patient = await _patientservice.getPatientById(patientId);
+    setState(() {
+      _editPatientName.text = patient[0]['name'] ?? 'No Name';
+      _editPatientDisease.text = patient[0]['diesease'] ?? 'No Disease';
+    });
+    _editPatientDialog(context);
+  }
+
+  _showSackbar(message) {
+    var _snackbar = SnackBar(
+      content: message,
+    );
+    _scaffoldKey.currentState.showSnackBar(_snackbar);
+  }
+
+  _deleteDialog(BuildContext context, patientId) {
+    return showDialog(
+        context: context,
+        builder: (param) {
+          return AlertDialog(
+            title: Text('Are you sure you want to delete'),
+            actions: [
+              FlatButton(
+                child: Text('Yes'),
+                onPressed: () async {
+                  await _patientservice.deletePatient(patientId);
+                  Navigator.pop(context);
+                  getAllPatients();
+                },
+              ),
+              FlatButton(
+                child: Text('No'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           _showFormDialog(context);
@@ -78,9 +195,46 @@ class _MyPatientsScreenState extends State<MyPatientsScreen> {
         child: Icon(Icons.add),
         backgroundColor: kPrimaryColor,
       ),
-      body: Body(),
+      body: ListView.builder(
+          itemCount: _patientList.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: ListTile(
+                leading: IconButton(
+                    icon: Icon(Icons.edit),
+                    onPressed: () {
+                      _editPatient(context, _patientList[index].id);
+                    }),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(_patientList[index].name),
+                    IconButton(
+                      icon: Icon(
+                        Icons.delete,
+                      ),
+                      onPressed: () {
+                        _deleteDialog(context, _patientList[index].id);
+                      },
+                    )
+                  ],
+                ),
+              ),
+            );
+          }),
       appBar: AppBar(
         title: Text('My Patients'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GestureDetector(
+                onTap: () {
+                  _auth.signOut();
+                  Navigator.pop(context);
+                },
+                child: Icon(Icons.close)),
+          )
+        ],
       ),
       drawer: Drawer(
           child: ListView(
